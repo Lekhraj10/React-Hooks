d99f25b0eb1961fd46927d768433a41ee2b6dd85
Lekhraj10 <lekhrajthapa10@gmail.com>
2025-01-10 15:07:14 +0545

    useEffect

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const increaseCount = () => {
-    setCount(count=>count+1)
-    setCount(count=>count+1)
-  }
+  useEffect (() => {
+    setTimeout(() => {
+      setCount(count => count+1)
+      
+    }, 1000);
 
+  },[])
 
   return (
     <>
-    <h1>Count : {count}</h1>
-    <button onClick={increaseCount}>Increase by 2</button>
+   <h1>I have rendered {count} times..</h1>
     </>     
   );
 }
