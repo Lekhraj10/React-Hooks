4f8abe8affc1be6da3d1aeace790bf46f2d8e71e
Lekhraj10 <lekhrajthapa10@gmail.com>
2025-01-10 14:17:23 +0545

    useState ex-2

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,21 +1,27 @@
+import React from "react";
 import { useState } from "react";
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState('Red');
+  const [car, setCar] = useState({
+    brand : "Eco Van",
+    model: "Maruti",
+    year: "2015",
+    color: "blue"
+  });
 
-  const changeColor = () => {
-    setColor('Blue');
+  const changeColor = ()=> {
+    setCar((car)=> {
+      return {...car, color: "white"}
+    })
   }
 
   return (
     <>
-    <h1>My Favorite color is {color}!</h1>
-    <button onClick={changeColor}>Blue</button>
-    </>
-    
-    
+    <h1>My {car.brand}</h1>
+    <h2>It is {car.color} color with {car.model} model and built in {car.year} year..</h2>
+    <button onClick={changeColor}>White</button>
+    </>     
   );
 }
-
 export default App;
