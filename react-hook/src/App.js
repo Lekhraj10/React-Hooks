5b2e0fb68fed7814bf6d3f145187abdc52b7d609
Lekhraj10 <lekhrajthapa10@gmail.com>
2025-01-11 16:08:15 +0545

    useReducer hook

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import './App.css'
-import Profile from './components/Profile';
-import Footer from './components/Footer';
-import Contact from './components/Contact';
+import './App.css';
+import { useReducer } from 'react';
 
 function App() {
+  
+  const initialState = {count : 0}
+
+  const reducer = (state, action) => {
+    switch(action.type){
+      case 'increase' : {
+        return {count: state.count + 1}
+      }
+      case 'decrease' : {
+        return {count: state.count -1}
+      }
+      case 'input' : {
+        return {count: action.payload}
+      }
+      default : {
+        return state.count
+      }
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <>
-    <Profile />
-    <Contact />
-    <Footer />
-
+    <h1>{state.count}</h1>
+    <button onClick={()=>dispatch({type: 'increase'})}>Increase</button>
+    <button onClick={()=>dispatch({type: 'decrease'})}>Decrease</button>
+    <br />
+    <input value={state.count} onChange={(e)=>dispatch({type: 'input',payload:Number(e.target.value)})} type="number" />
+    
     </>     
   );
 }
